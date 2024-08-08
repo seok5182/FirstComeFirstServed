52b7fda80fad31eec761d58f5b8c69959d58e434
seok5182 <seok5182@naver.com>
2024-08-08 09:52:05 +0900

    Init commit

---
+++ src/main/java/com/sparta/firstcomefirstserved/FirstComeFirstServedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.firstcomefirstserved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstComeFirstServedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstComeFirstServedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/firstcomefirstserved/FirstComeFirstServedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.firstcomefirstserved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstComeFirstServedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
