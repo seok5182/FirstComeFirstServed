06191ad2f0e01c50d1040e697d3e89e8f0623b74
seok5182 <seok5182@naver.com>
2024-08-13 16:24:04 +0900

    장바구니 구현 1. 장바구니에 상품 담기 2. 장바구니 전체 상품 조회 3. 장바구니에 있는 상품 상세 페이지 4. 장바구니에 담겨 있는 상품 수량 변경 5. 장바구니에서 상품 제거

---
+++ src/main/java/com/sparta/firstcomefirstserved/repository/CartRepository.java
@@ -0,0 +1,17 @@
+package com.sparta.firstcomefirstserved.repository;
+
+import com.sparta.firstcomefirstserved.entity.Cart;
+import com.sparta.firstcomefirstserved.entity.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+	Optional<List<Cart>> findAllByUser(User user);
+
+	Page<Cart> findAllByUser(User user, Pageable pageable);
+
+}
